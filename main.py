6bacdfe975bda06e46575e21c7d170450a2c652d
lazaryan <lazaryan99@mail.ru>
2019-04-14 22:14:05 +0300

    garbage

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
-# from app import app
-# import view
-# import rest_todo
+from app import app
+import view
+import rest_todo
 
-import os
-from time import sleep
-
-
-# from Essenses.decorators import thread
-# from Essenses.block import Block
+from Essenses.decorators import thread
+from Essenses.block import Block
 
 
-# @thread
-# def server():
-#     block = Block()
-#     block.create_block()
+@thread
+def server():
+    block = Block()
+    block.create_block()
 
 
 if __name__ == '__main__':
-    # server()
-    #app.run()
-    with open('data/test.txt', 'r') as f:
-        a = f.readlines()
-        print(len(a))
-        os.system('cls')
-
-    count = 0
-
-    os.system('start python test.py')
-    # os.startfile('./test.py')
-
-    while True:
-        print(a)
-        print('test')
-        print('log')
-        sleep(5)
-        os.system('cls')
-        count += 1
+    server()
+    app.run()
